305fce941c23d447df3d76a976ff2ea65ba5ab0d
Florian <florian@bildspur.ch>
2019-08-21 12:48:38 +0200

    implemented device info

--- src/main/java/org/intel/rs/Device.java
+++ src/main/java/org/intel/rs/Device.java
@@ -10,6 +10,22 @@ public class Device implements AutoCloseable {
         this.instance = instance;
     }
 
+    public String getDeviceInfo(rs2_camera_info info) {
+        // check if info is supported
+        rs2_error error = new rs2_error();
+        boolean isSupported = toBoolean(rs2_supports_device_info(instance, info, error));
+        checkError(error);
+
+        if(!isSupported)
+            return null;
+
+        // read device info
+        String infoText = rs2_get_device_info(instance, info, error).getString();
+        checkError(error);
+
+        return infoText;
+    }
+
     @Override
     public void close() {
         rs2_delete_device(instance);
--- src/test/java/org/inte/rs/test/DeviceTest.java
+++ src/test/java/org/inte/rs/test/DeviceTest.java
@@ -1,7 +1,9 @@
 package org.inte.rs.test;
 
 import org.intel.rs.Context;
+import org.intel.rs.Device;
 import org.intel.rs.DeviceList;
+import org.intel.rs.api.RealSense;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -25,6 +27,17 @@ public class DeviceTest {
         DeviceList list = context.queryDevices();
         int count = list.getCount();
         System.out.println("Device Count: " + count);
+    }
+
+    @Test
+    public void getDeviceInfo() {
+        DeviceList list = context.queryDevices();
+        int count = list.getCount();
+        if(count < 0) return;
+
+        Device device = list.get(0);
+        String info = device.getDeviceInfo(RealSense.rs2_camera_info.RS2_CAMERA_INFO_NAME);
+        System.out.println("Name: " + info);
     }
 
 }
