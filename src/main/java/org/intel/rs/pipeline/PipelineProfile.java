684872c1ba3eb5944fd86b866b5f4c3b9bd4bbc4
Florian <florian@bildspur.ch>
2019-12-26 19:11:06 +0100

    implemented stream profile

--- src/main/java/org/intel/rs/pipeline/PipelineProfile.java
+++ src/main/java/org/intel/rs/pipeline/PipelineProfile.java
@@ -5,6 +5,9 @@ import static org.bytedeco.librealsense2.global.realsense2.*;
 
 import org.bytedeco.librealsense2.*;
 import org.intel.rs.Context;
+import org.intel.rs.device.Device;
+import org.intel.rs.stream.StreamProfileList;
+import org.intel.rs.types.Stream;
 import org.intel.rs.util.NativeDecorator;
 
 public class PipelineProfile implements NativeDecorator<rs2_pipeline_profile> {
@@ -13,6 +16,22 @@ public class PipelineProfile implements NativeDecorator<rs2_pipeline_profile> {
     public PipelineProfile(rs2_pipeline_profile instance) {
         this.instance = instance;
     }
+
+    public Device getDevice() {
+        rs2_error error = new rs2_error();
+        rs2_device ptr = rs2_pipeline_profile_get_device(instance, error);
+        checkError(error);
+        return new Device(ptr);
+    }
+
+    public StreamProfileList getStreams() {
+        rs2_error error = new rs2_error();
+        rs2_stream_profile_list ptr = rs2_pipeline_profile_get_streams(instance, error);
+        checkError(error);
+        return new StreamProfileList(ptr);
+    }
+
+    // todo: implement get stream
 
     @Override
     public rs2_pipeline_profile getInstance() {
