f35106cbbfc8f0c6667199418c104c3013a4cfed
Florian <florian@bildspur.ch>
2019-12-31 17:55:39 +0100

    implemented options to processing block

--- src/main/java/org/intel/rs/processing/ProcessingBlock.java
+++ src/main/java/org/intel/rs/processing/ProcessingBlock.java
@@ -1,23 +1,31 @@
 package org.intel.rs.processing;
 
-import static org.intel.rs.util.RealSenseUtil.checkError;
-import static org.bytedeco.librealsense2.global.realsense2.*;
-
-import org.bytedeco.librealsense2.*;
-import org.intel.rs.Context;
-import org.intel.rs.device.Device;
-import org.intel.rs.stream.StreamProfileList;
-import org.intel.rs.types.Stream;
+import org.bytedeco.librealsense2.rs2_processing_block;
+import org.bytedeco.librealsense2.rs2_sensor;
+import org.intel.rs.option.SensorOptions;
 import org.intel.rs.util.NativeDecorator;
+
+import static org.bytedeco.librealsense2.global.realsense2.rs2_delete_processing_block;
 
 public class ProcessingBlock implements NativeDecorator<rs2_processing_block> {
     rs2_processing_block instance;
+    private SensorOptions options;
 
     public ProcessingBlock() {
     }
 
     public ProcessingBlock(rs2_processing_block instance) {
         this.instance = instance;
+    }
+
+    public SensorOptions getOptions() {
+        // todo: make thread safe
+        if (options == null) {
+            rs2_sensor sensor = new rs2_sensor(instance);
+            options = new SensorOptions(sensor);
+        }
+
+        return options;
     }
 
     @Override
