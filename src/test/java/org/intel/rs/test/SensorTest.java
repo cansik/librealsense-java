1232129790edc737a0033f9a38e24d37dc1b6935
Florian Bruggisser <florian@bildspur.ch>
2020-09-21 13:55:34 +0200

    implemented basic sensor option setting

--- src/test/java/org/intel/rs/test/SensorTest.java
+++ src/test/java/org/intel/rs/test/SensorTest.java
@@ -1,9 +1,13 @@
 package org.intel.rs.test;
 
-import org.intel.rs.*;
+import org.intel.rs.Context;
 import org.intel.rs.device.Device;
 import org.intel.rs.device.DeviceList;
+import org.intel.rs.option.CameraOption;
+import org.intel.rs.sensor.Sensor;
 import org.intel.rs.sensor.SensorList;
+import org.intel.rs.types.Option;
+import org.intel.rs.types.Stream;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -40,5 +44,46 @@ public class SensorTest {
         int count = sensors.count();
         System.out.println("SensorCount: " + count);
         sensors.count();
+    }
+
+    @Test
+    public void listSettings() {
+        if(device == null)
+            return;
+
+        SensorList sensors = device.querySensors();
+
+        for(Sensor sensor : sensors) {
+            System.out.println("Sensor " + sensor.getName());
+
+            for(CameraOption option : sensor.getSensorOptions()) {
+                System.out.println("\t " + option + " Value: " + option.getValue() + " (" + option.getDescription() + ")");
+            }
+        }
+    }
+
+    @Test
+    public void setAutoExposureOption() {
+        if(device == null)
+            return;
+
+        SensorList sensors = device.querySensors();
+
+        for(Sensor sensor : sensors) {
+            // get color sensor as shown in the official example
+            // (https://github.com/robotology/yarp/pull/2010/commits/74734a46ed92bac71d921922c0ace7d0543e69ad#diff-b2d325b49d86019f0568d92345413a79R72)
+            if(sensor.getStreamProfiles().get(0).getStream() == Stream.Color) {
+                System.out.println("setting auto exposure for " + sensor.getName() + "...");
+                CameraOption option = sensor.getSensorOptions().get(Option.EnableAutoExposure);
+
+                if(!option.isSupported()) {
+                    System.out.println("option not supported!");
+                    continue;
+                }
+
+                option.setValue(0.0f);
+                System.out.println("done!");
+            }
+        }
     }
 }
